14db328eea12e1ae40e330f0656eb6f2e2f5f16e
Matthew Stolley <mstolley@crocs.com>
2025-04-07 19:26:32 -0600

    center title in Header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,12 +18,12 @@ function Header({ localHighScore, winner, wins }: HeaderProps) {
     return (
         <header className="fixed top-0 left-0 w-full z-20 h-14">
             <div className="flex justify-between items-center p-2.5 bg-gray-300 dark:bg-gray-800">
-                <div className="flex items-center">
+                <div className="flex items-center w-46">
                     <span className="mr-2.5">Wins</span>
                     <span className="w-10 font-bold rounded-sm py-0 px-1.25 text-green-800 bg-green-100">{wins}</span>
                 </div>
                 <h1 className="text-3xl font-bold text-gray-500 dark:text-gray-400">GotGame</h1>
-                <div className="flex items-center">
+                <div className="flex items-center w-46">
                     <div className="flex justify-center items-center h-full p-2.5 gap-2.5 border border-gray-300 dark:border-gray-700 rounded-md mr-2.5">
                         <input
                             className="hover:cursor-pointer"
